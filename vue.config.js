5ba27272a503b3fc0c75a5f2ea505a4a989ab281
ChandralingamP <chandralingam2003@gmail.com>
2023-03-12 21:27:49 +0530

    dist

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-// module.exports = {
-//   devServer: {
-//     public: '0.0.0.0:8080'
-//   }
-// }
